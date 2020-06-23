eb96c621b3e4fd7f2acdb365132f6b250999f4ba
Caio Ledesma <42016457+caioledesma@users.noreply.github.com>
2020-06-23 15:49:22 -0300

    added the js  modal functionality

--- challenge3-foodfy/solution-caio/script.js
+++ challenge3-foodfy/solution-caio/script.js
@@ -6,16 +6,18 @@ for(let card of cardsSelect){
     card.addEventListener('click', function() {
         const cardId = card.getAttribute('id')
         const cardTitle = card.querySelector('h3').innerHTML
+        const cardName = card.querySelector('p').innerHTML
         findModal.querySelector('img').src = `assets/${cardId}.png`
         findModal.querySelector('h3').innerHTML = cardTitle 
+        findModal.querySelector('p').innerHTML = cardName
         findModal.classList.add('active')
         // console.log(cardText)
     })
 }
 
-
 document.querySelector('.close-modal').addEventListener('click', function(){
     findModal.classList.remove('active')
+    
 })
 
 
