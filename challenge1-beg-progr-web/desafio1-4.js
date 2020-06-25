d6082f042a12dffc97aaebef8d8e92281a2ddead
Caio Ledesma <42016457+caioledesma@users.noreply.github.com>
2020-06-24 21:46:56 -0300

    added new functions on challenge 1-4

--- challenge1-beg-progr-web/desafio1-4.js
+++ challenge1-beg-progr-web/desafio1-4.js
@@ -27,17 +27,47 @@ createTransaction({ type: "debit", value: 30 });
 console.log(user.transactions)
 console.log(user.balance)
 
+// Crie uma função chamada getHigherTransactionByType que recebe como parâmetro o tipo de transação credit/debit, percorre as transações do usuário e retorna o objeto da transação de maior valor com aquele tipo:
+// getHigherTransactionByType("credit"); // { type: 'credit', value: 120 }
 
-function getHigherTransactionByType(xxx){
-    return Math.max.apply(Math, xxx)   
-}
-function getAverageTransactionValue(yyy){
+
+// function getHigherTransactionByType(xxx){ 
+//     let higher = Math.max.apply(Math, xxx)
+//     if(higher == user.transactions.value){
+//         console.log(`o maior é ${user.transactions[higher.value]}`)    
+//         return higher
+//     }    
     
-    return user.balance =+ transaction.value / (user.transactions.length)
-}
+// } 
 
-console.log(getAverageTransactionValue(user.transactions))
-console.log(getHigherTransactionByType(user.transactions))
+
+
+console.log(Max.math.apply(Math, user.transactions))
+// console.log(getHigherTransactionByType(user.transactions))
+
+// console.log(user.transactions.value)
+
+// primeiro eu descubro ql é maior, depois pego esse maior e comparo com as transações, quando for igual é o higher transaction, msm coisa pro mínimo 
+// if higherTransaction = user.transactions.value 
+// return user.transaction
+
+
+// if (user.transaction.type == "credit"){
+//     for (i=0; i > )
+//     if(user.transaction[i] > user.transaction[i+1]){
+//         return true 
+//     }
+
+// }
+
+
+
+// function getAverageTransactionValue(yyy){
+    
+//     return user.balance =+ transaction.value / (user.transactions.length)
+// }
+
+// console.log(getAverageTransactionValue(user.transactions))
 
 
 // createTransaction(transaction.type, transaction.value)
