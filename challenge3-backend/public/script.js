8c5c9398007e09cbe36438965ddb75f4d080776e
Caio Ledesma <42016457+caioledesma@users.noreply.github.com>
2020-06-29 18:12:31 -0300

    new version of challenge 3

--- challenge3-backend/public/script.js
+++ challenge3-backend/public/script.js
@@ -4,15 +4,23 @@ const cards = document.querySelectorAll('.card')
 for (let card of cards){
     card.addEventListener('click', function(){
         const cardId = card.getAttribute('id')
-        modalGeral.classList.add('active')
-        modalGeral.querySelector('iframe').src = `https://rocketseat.com.br/${cardId}`
-        modalGeral.classList.contains('.modal')
+       window.location.href = `/solo/${cardId}`
     })
 }
 
-document.querySelector('.close-modal').addEventListener('click',function(){
-    modalGeral.classList.remove('active')
-})
 
 
     
+// for (let card of cards){
+//     card.addEventListener('click', function(){
+//         const cardId = card.getAttribute('id')
+//         modalGeral.classList.add('active')
+//         modalGeral.querySelector('iframe').src = `https://rocketseat.com.br/${cardId}`
+//         modalGeral.classList.contains('.modal')
+//     })
+// }
+
+// document.querySelector('.close-modal').addEventListener('click',function(){
+//     modalGeral.classList.remove('active')
+// })
+
--- challenge3-backend/server.js
+++ challenge3-backend/server.js
@@ -22,6 +22,23 @@ server.get("/", function(req,res) {
     return res.render("about")
  }) 
 
+server.get("/solo/:id", function(req,res){
+    const id = req.params.id;
+
+    // const name = res.send(id)
+    const card = data_courses.find(function(card){
+        if (card.name == id){
+            return true
+        }
+    })
+    if (!card){
+        return res.status(404).render("not-found")
+    }
+
+    console.log(card)
+    return res.render("solo", {course: card});
+});
+
  // uma forma de fazer o 404 só pra digitação errada na URL
 //  server.get("/*", function(req,res) {
 //     return res.render("not-found")
